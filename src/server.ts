d4b329463276255b49b3c528e942a37c68bc5d54
rafsan102 <ahmedrafsan101@gmail.com>
2026-08-16 23:37:37 +0600

    make a post method in the server

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,17 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!');
+  res.send('next level web World!');
 });
 
+app.post('/', (req: Request, res: Response) => {
+  console.log(req);
+
+  res.status(201).json({
+    success: true,
+    message:'API is worked'
+  })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
